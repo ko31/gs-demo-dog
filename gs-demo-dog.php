cb6b478e3a92157d93ba160f9eb4e9fc08a654e6
Ko Takagi <knockoutmarch@gmail.com>
2021-12-19 14:17:41 +0900

    :wrench: First commit

---
+++ gs-demo-dog.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:     Dog API demo
+ * Plugin URI:      https://github.com/ko31/gs-demo-dog
+ * Description:     This is a sample plugin to receive Dog API data.
+ * Author:          ko31
+ * Author URI:      https://go-sign.info
+ * Text Domain:     gs-demo-dog
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Gs_Demo_Dog
+ */
+
+namespace Gs_Demo_Dog;
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
